265345e10de8672a9c88fb84b217bb936da6d8f1
AhmedBinhamim <ahmedbinhamim@gmail.com>
2024-06-03 16:44:29 +0300

    updated port to forward to render web services

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 8081;
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -34,6 +35,6 @@ app.get('/products', (req, res) => {
     });
 });
 
-app.listen(8081, () => {
+app.listen(port, () => {
   console.log('Listening on port 8081');
 });
